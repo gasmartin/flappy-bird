d19f1b5c851c73427760bc1cfcee180e218b2a9b
Gabriel San Martin <gssm.snf19@uea.edu.br>
2019-11-03 23:32:43 -0400

    Inicializando a janela

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import pygame
 
-print("Hello World")
+screen_width = 800
+screen_height = 600
+screen_mode = (screen_width, screen_height)
+
+screen = pygame.display.set_mode(screen_mode)
+
+playing = True
+while playing:
+    # Eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            playing = False
+
+    pygame.display.update()
