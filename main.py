413d6c3373d0c43e904e15074cd3eb6557b53e85
Gabriel San Martin <gssm.snf19@uea.edu.br>
2019-11-05 17:25:17 -0400

    Controlando o fps

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ screen_width = 576
 screen_height = 512
 screen_mode = (screen_width, screen_height)
 
+window_fps = 60
+
 pygame.display.init()
 screen = pygame.display.set_mode(screen_mode)
 pygame.display.set_caption("Flappy Bird")
+
+clock = pygame.time.Clock()
 
 background_image = pygame.image.load("assets/sprites/background-day.png")
 
@@ -15,6 +19,11 @@ base_x = 0
 
 player_image = pygame.image.load("assets/sprites/yellowbird-midflap.png")
 player_x, player_y = 170, 200
+player_dy = 1
+player_angle = 0
+
+def jump():
+    print("JUMP")
 
 def draw_background():
     screen.blit(background_image, (0, 0))
@@ -28,16 +37,35 @@ def draw_bases():
     if base_x <= -96:
         base_x = 0
 
+def update_player():
+    # player_y += player_dy
+    pass
+
 def draw_player():
+    global player_y
     screen.blit(player_image, (player_x, player_y))
 
 playing = True
 while playing:
-    # Eventos
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    for event in events:
         if event.type == pygame.QUIT:
             playing = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                jump()
+
+    screen.fill((0, 0, 0))
+
     draw_background()
     draw_bases()
+
+    update_player()
     draw_player()
+
     pygame.display.update()
+    clock.tick(window_fps)
+
+    print("FPS:", clock.get_fps())
+
+pygame.quit()
