20bc369294c2df231289f1d7e7de66df889208a9
Gabriel San Martin <gssm.snf19@uea.edu.br>
2019-11-04 21:35:15 -0400

    Movimentaçao do cenario ok

--- main.py
+++ main.py
@@ -6,12 +6,24 @@ screen_mode = (screen_width, screen_height)
 
 pygame.display.init()
 screen = pygame.display.set_mode(screen_mode)
+pygame.display.set_caption("Flappy Bird")
 
 background_image = pygame.image.load("assets/sprites/background-day.png")
+
+base_image = pygame.image.load("assets/sprites/base.png")
+base_x = 0
 
 def draw_background():
     screen.blit(background_image, (0, 0))
     screen.blit(background_image, (288, 0))
+
+def draw_bases():
+    global base_x
+    screen.blit(base_image, (base_x, 400))
+    screen.blit(base_image, (base_x + 336, 400))
+    base_x -= 1
+    if base_x <= -96:
+        base_x = 0
 
 playing = True
 while playing:
@@ -20,4 +32,5 @@ while playing:
         if event.type == pygame.QUIT:
             playing = False
     draw_background()
+    draw_bases()
     pygame.display.update()
